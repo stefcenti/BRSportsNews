5de7fa9eb4fa55e2eb2e7bb37126c75beb108f0f
Stefanie Centi <stefcenti@centiumtech.com>
2016-09-08 11:42:29 -0400

    Initial files for scrapping app

---
+++ models/Article.js
@@ -0,0 +1,39 @@
+// require mongoose
+var mongoose = require('mongoose');
+// create Schema class
+var Schema = mongoose.Schema;
+
+// Create article schema
+var ArticleSchema = new Schema({
+  // sport name is required
+  sportName: {
+    type:String,
+    required:true
+  },
+  // sportLink is required
+  sportLink: {
+    type:String,
+    required:true
+  },
+  // articleHeadline is required
+  articleHeadline: {
+    type:String,
+    required:true
+  },
+  // articleLink is required
+  articleLink: {
+    type:String,
+    required:true
+  },
+  // this only saves one note's ObjectId. ref refers to the Note model.
+  note: {
+      type: Schema.Types.ObjectId,
+      ref: 'Note'
+  }
+});
+
+// Create the Article model with the ArticleSchema
+var Article = mongoose.model('Article', ArticleSchema);
+
+// export the model
+module.exports = Article;
